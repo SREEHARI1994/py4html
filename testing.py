a5f94c9f3a5b7474f8cfc5ea52076e3432a1d1f7
SREEHARI1994 <sreehari.mras@gmail.com>
2024-08-23 01:21:42 +0530

    testing file added

---
+++ testing.py
@@ -0,0 +1,10 @@
+from py4html import *
+
+start("testing.html")
+heading(1,"just trying")
+paragraph("different implementations")
+division_begins()
+heading(2,"this comes inside the division")
+heading(3,"this too comes inside the division")
+division_ends()
+end()
